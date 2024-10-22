c499b88201751de599690216a9e721669e5167fe
frank <frank@aheadintranet.com>
2024-10-22 15:45:53 +0200

    seems to be necessary

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://aheadintranet.github.io",
+  base: "/api-documentation",
   integrations: [
     starlight({
       title: "API Documentation",
